4e44b46d4e3eac2c1798ecceec2ccc42d03ca85a
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-15 15:13:48 +0200

    [UPD] MAPPATURA DELLE ROTTE

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './links/Homepage';
 
 function App() {
 
   return (
     <>
       {/* ANDIAMO AD USARE LA COMPONENTE BrowserRouter */}
-      <Browser></Browser>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={Homepage}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 };
