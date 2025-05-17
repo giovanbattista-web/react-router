4ffa7d6996c736098d3933878ca21572c49d2291
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-17 07:22:37 +0200

    [UPD] IMPORTO AXIOS, USESTATE ED useEffect PER POI RECUPERARE LE INFORMAZIONI

--- src/elenco/Detail.jsx
+++ src/elenco/Detail.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 
 const Detail = () => {
     return (
         <div>
-        <h3>Hello World </h3>
-            </div>
+            <h3>Hello World </h3>
+        </div>
     )
 };
 export default Detail;
